23e41d87a524820adef80b95cd35c2759faa0a66
isamuyun <yunchunnan@qq.com>
2024-02-13 11:16:55 -0800

    Project for data visualization with ArcGIS

---
+++ geocoding.py
@@ -0,0 +1,10 @@
+import googlemaps
+import csv
+
+gmaps = googlemaps.Client('AIzaSyBv1hiFGq4PI5tKUOQ_FhXaqHXM4zVunTs')
+
+# Geocoding an address
+#geocode_result = gmaps.geocode("4380 Alexander NE, Ste E&F, Albuquerque, NM, 87107")
+
+#print(geocode_result[0]["geometry"]["location"])
+
